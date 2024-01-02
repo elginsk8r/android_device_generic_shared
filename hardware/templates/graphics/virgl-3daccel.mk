2f49e8e59ffb551b1ed354867e4c60940a767d43
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-03 05:41:03 +0800

    hw: templates: Add graphics virgl templates
    Change-Id: Ia05ab98973f84b85f7e646fd7d8ef8d75731f46a

---
+++ hardware/templates/graphics/virgl-3daccel.mk
@@ -0,0 +1,5 @@
+# Reference: device/google/cuttlefish/shared/graphics/device_vendor.mk
+$(call inherit-product, device/generic/shared/hardware/graphics/composer/drm_hwcomposer/drm_hwcomposer.mk)
+$(call inherit-product, device/generic/shared/hardware/graphics/gralloc/minigbm_aidl/minigbm_aidl.mk)
+
+$(call inherit-product, device/generic/shared/hardware/graphics/egl/mesa/virgl.mk)
