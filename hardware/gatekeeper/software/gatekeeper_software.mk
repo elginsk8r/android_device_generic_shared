a07f15f4d45ed3dc1bb1701c1f829332efd53200
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-03 05:53:39 +0800

    hw: gatekeeper: software: Add software gatekeeper configuration
    Change-Id: Ib1a61afee63b3e99537e7668a3789bb927f28d17

---
+++ hardware/gatekeeper/software/gatekeeper_software.mk
@@ -0,0 +1,5 @@
+# Reference: device/generic/goldfish/vendor_common.mk
+
+BOARD_VENDOR_SEPOLICY_DIRS += device/generic/shared/hardware/gatekeeper/software/sepolicy/vendor
+
+PRODUCT_PACKAGES += android.hardware.gatekeeper@1.0-service.software
