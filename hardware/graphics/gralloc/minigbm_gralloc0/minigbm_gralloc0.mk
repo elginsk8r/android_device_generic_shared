e163d034a2d99fc3cb45d3575748bcaea402b3f8
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-02 20:48:37 +0800

    hw: graphics: gralloc: Add minigbm_gralloc0 configuration
    Change-Id: I92c3a7c73be676ccc256670d3ae06dff27c9302b

---
+++ hardware/graphics/gralloc/minigbm_gralloc0/minigbm_gralloc0.mk
@@ -0,0 +1,15 @@
+# Reference: device/generic/goldfish/fvp.mk
+
+BOARD_VENDOR_SEPOLICY_DIRS += device/generic/shared/hardware/graphics/gralloc/minigbm_gralloc0/sepolicy/vendor
+
+DEVICE_MANIFEST_FILE += device/generic/shared/hardware/graphics/gralloc/minigbm_gralloc0/minigbm_gralloc0.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    gralloc.minigbm
+
+# Reference: device/generic/goldfish/fvpbase/init.qemu.rc
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.gralloc?=minigbm
