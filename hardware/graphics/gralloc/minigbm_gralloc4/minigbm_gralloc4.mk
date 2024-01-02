9ca86d42bfe90726692677a49a90b14d81d10f69
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-02 21:29:40 +0800

    hw: graphics: gralloc: minigbm_gralloc4: Add support for variants
    Change-Id: If0ad521b8ada24562957a3aba2d86e78ff942817

--- hardware/graphics/gralloc/minigbm_gralloc4/minigbm_gralloc4.mk
+++ hardware/graphics/gralloc/minigbm_gralloc4/minigbm_gralloc4.mk
@@ -3,5 +3,5 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/generic/shared/hardware/graphics/gralloc/minigbm_gralloc4/sepolicy/vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@4.0-service.minigbm \
-    android.hardware.graphics.mapper@4.0-impl.minigbm
+    android.hardware.graphics.allocator@4.0-service.minigbm$(TARGET_GRALLOC4_MINIGBM_VARIANT) \
+    android.hardware.graphics.mapper@4.0-impl.minigbm$(TARGET_GRALLOC4_MINIGBM_VARIANT)
