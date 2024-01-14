14e5ba2e2996871eaea94a8e82a31fcfc9d6d99e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-14 16:21:06 +0800

    hw: graphics: composer: drm_hwcomposer: Add support for 2.3 HIDL
    Change-Id: I00cf2714119473a9a8372ada1a4c7efdb10d69b1

--- hardware/graphics/composer/drm_hwcomposer/drm_hwcomposer.mk
+++ hardware/graphics/composer/drm_hwcomposer/drm_hwcomposer.mk
@@ -1,13 +1,27 @@
-# Reference: device/google/cuttlefish/shared/graphics/device_vendor.mk
+# References:
+# device/linaro/dragonboard/full.mk
+# device/google/cuttlefish/shared/graphics/device_vendor.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/generic/shared/hardware/graphics/composer/drm_hwcomposer/sepolicy/vendor
 
+TARGET_HWC2_HIDL_VERSION ?= 2.4
+ifeq ($(TARGET_HWC2_HIDL_VERSION),2.4)
 DEVICE_MANIFEST_FILE += \
     device/google/cuttlefish/shared/config/manifest_android.hardware.graphics.composer@2.4-service.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-service \
+    android.hardware.graphics.composer@2.4-service
+else ifeq ($(TARGET_HWC2_HIDL_VERSION),2.3)
+DEVICE_MANIFEST_FILE += \
+    device/generic/shared/hardware/graphics/composer/drm_hwcomposer/drm_hwcomposer_2_3.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-impl \
+    android.hardware.graphics.composer@2.3-service
+endif
+
+PRODUCT_PACKAGES += \
     hwcomposer.drm$(TARGET_HWC2_DRM_VARIANT)
 
 PRODUCT_VENDOR_PROPERTIES += \
